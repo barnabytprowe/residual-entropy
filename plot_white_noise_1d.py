2cf16986c90e1ef03dd9eea07b87b2024826971b
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2018-11-24 23:05:22 -0800

    added plots of individal cases

--- plot_white_noise_1d.py
+++ plot_white_noise_1d.py
@@ -5,22 +5,77 @@ plot_white_noise_1d.py
 Plot examples of fitting white noise in 1D, using stored results created by the script
 fitting_white_noise_1d.py
 """
+import os
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-# if False:
-#     # Show x, y results
-#     plt.plot(x, y)
-#     plt.plot(x, yfit)
-#     plt.show()
 
-#     # Show x, y results
-#     plt.bar(x, r, width=0.01)
-#     plt.show()
+# Ready output folder
+if not os.path.isdir(os.path.join(".", "plots")):
+    os.mkdir(os.path.join(".", "plots"))
 
-# if False:
-#     # Plot the pure noise and residual correlation functions
-#     plt.bar(np.arange(n//2) - 0.2, rcf[:n//2], width=0.4)
-#     plt.bar(np.arange(n//2) + 0.2, ncf[:n//2], width=0.4)
-#     plt.show()
+# Load detailed ("all") results from pickled file object
+with open("wn1d.1e3.all.pickle", "r") as fin:
+    rall = pickle.load(fin)
+# Pull out results
+yall = rall["yall"]
+yfit = rall["yfit"]
+rcf = rall["rcf"]
+ncf = rall["ncf"]
+
+# Define unit interval
+x = np.linspace(-.5, .5, num=yall.shape[-1], endpoint=False)
+
+print("Sum of Square Errors (SSE) results for basic y, fit and residuals examples")
+print("--------------------------------------------------------------------------")
+# Loop through orders to show in the example figures - something low -> intermediate -> high
+for m, fig_str in zip((1, 21, 41), ("fig1", "fig2", "fig3")):
+
+    print("m = "+str(m))
+    fig = plt.figure(figsize=(6, 4))
+    plt.grid()
+    plt.plot(x, yall[m, 0, :], label=r"White noise $\sim N(0, 1)$")
+    plt.plot(x, yfit[m, 0, :], label=r"Best-fitting curve for $m="+str(m)+"$")
+    plt.ylim(-3, 3)
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+    plt.legend(loc=2)
+    plt.tight_layout()
+    plt.savefig(os.path.join(".", "plots", fig_str+"a.pdf"))
+    plt.close(fig)
+
+    fig = plt.figure(figsize=(6, 4))
+    print("SSE of y (no fit applied) = "+str(np.sum(yall[m, 0, :]**2)))
+    plt.grid()
+    #plt.plot(x, yall[m, 0, :], label=r"White noise $\sim N(0, 1)$")
+    plt.plot(
+        x, yall[m, 0, :] - yfit[m, 0, :], "r--",
+        label=r"Residual from best-fitting curve for $m="+str(m)+"$")
+    plt.ylim(-3, 3)
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$y$")
+    plt.legend(loc=2)
+    plt.tight_layout()
+    plt.savefig(os.path.join(".", "plots", fig_str+"b.pdf"))
+    plt.clf()
+    print("SSE of fit (m="+str(m)+") residuals = "+str(np.sum((yall[m, 0, :] - yfit[m, 0, :])**2)))
+    plt.close(fig)
+
+# Loop through orders to show the rcf examples
+mmax = 1 + ncf.shape[-1]//2
+fig = plt.figure()
+plt.grid()
+for m in (1, 21, 41):
+
+    plt.plot(
+        rcf[m, 0, :mmax], label=r"$m="+str(m)+"$",
+        ls={1: "-", 21: "--", 41: ":"}[m])
+    plt.ylim(-1, 1.1)
+    plt.xlabel(r"$|\Delta x|$")
+    plt.ylabel("Autocorrelation function")
+    plt.legend(loc=1)
+
+plt.tight_layout()
+plt.show()
+plt.close(fig)
