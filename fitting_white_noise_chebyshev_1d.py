46b714aad89d32d833877633100500696b809dd7
Barnaby Rowe <barnaby.t.p.rowe@gmail.com>
2019-05-23 14:06:03 +0100

    edits made to understand the 'ringing' phenomena - seems to be due to aliasing of the sub-pixel scale variations in the Chebyshev functions beyond a certain order

--- fitting_white_noise_chebyshev_1d.py
+++ fitting_white_noise_chebyshev_1d.py
@@ -10,8 +10,8 @@ from numpy.polynomial.chebyshev import chebval
 
 # Setup parameters
 # ----------------
-output_filename = "wnc1d.1e3.all.pickle"
-store_all = True # Set True to store all y values, all best-fitting y model values, all correl fns
+output_filename = "wnc1d.1e3.pickle"
+store_all = False # Set True to store all y values, all best-fitting y model values, all correl fns
                   # ...don't set with Nruns = 100000 unless you have plenty of memory!
 # Number of data points
 nx = 100
@@ -126,6 +126,10 @@ for im in range(mmax):
     med_lncps.append(np.median(mean_lncps_over_k))
     lqt_lncps.append(np.quantile(mean_lncps_over_k, 0.05))
     uqt_lncps.append(np.quantile(mean_lncps_over_k, 0.95))
+
+    #if im > 59:
+    #    import matplotlib.pyplot as plt
+    #    import ipdb; ipdb.set_trace()
 
     if store_all:
         rcpsd.append(rcps)
